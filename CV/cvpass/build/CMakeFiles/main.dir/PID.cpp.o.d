9bc13c173b9f69d0440499df48b5a5053497a183
MES10RE <l03030827@163.com>
2025-07-10 13:14:57 +0800

    重新初始化提交

---
+++ CV/cvpass/build/CMakeFiles/main.dir/PID.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/main.dir/PID.cpp.o: /home/handle/loongson/ABI1/cvpass/PID.cpp \
+ /opt/loongarch64-gnu-gcc-ABI1/loongarch64-linux-gnu/sysroot/usr/include/stdc-predef.h
---
+++ README.md
@@ -0,0 +1,1 @@
+# Loongson-IVAD
